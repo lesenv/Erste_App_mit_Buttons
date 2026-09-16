ec43a6fc50a92a32a146449aef7f7016b9d3dbec
lesenv <johndoe@example.com>
2026-09-16 13:10:38 +0200

    layout done, next connecting logic

--- app/src/main/java/com/lesenv/ersteappmitbuttons/MainActivity.kt
+++ app/src/main/java/com/lesenv/ersteappmitbuttons/MainActivity.kt
@@ -2,11 +2,14 @@ package com.lesenv.ersteappmitbuttons
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.lesenv.ersteappmitbuttons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var btn: FloatingActionButton
+    private var result: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
